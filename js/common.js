7742cb1a86f5574bacfc02b835c6119cfb2d9e9c
emichika <emichika20090401@gmail.com>
2021-01-13 23:25:47 +0900

    保守対応 表示データの更新
    バージョンアップ ver.2.3.4
    前回のバグ改修分のバージョンアップも含む

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-// 参考
+// 参考 https://proverb-encyclopedia.com/subarasi/
 // 日本語リスト
 const japaneseList = [
   "青は藍より出でて藍より青し",
@@ -6,6 +6,11 @@ const japaneseList = [
   "後の雁が先になる",
   "鞍上人なく、鞍下馬なし",
   "いずれ菖蒲か杜若",
+  "一字千金",
+  "一を聞いて十を知る",
+  "一騎当千",
+  "一頭地を抜く",
+  "上には上がある",
 ];
 
 // ローマ字リスト
@@ -15,10 +20,15 @@ const romajiList = [
   "ATONOKARIGASAKININARU",
   "ANNJYOUHITONAKU,ANNKAUMANASI",
   "IZUREAYAMEKAKAKITUBATA",
+  "ITIJISENNKINN",
+  "ITIWOKIITEJYUUWOSIRU",
+  "IKKITOUSENN",
+  "ITTOUTIWONUKU",
+  "UENIHAUEGAARU",
 ];
 
 const copr = "&copy; Copyright emichika Inc. All Rights <span id='ver'></span>";
-const verNum = "2.2.3";
+const verNum = "2.3.4";
 
 function setCopyright() {
   copyright.innerHTML = copr;
