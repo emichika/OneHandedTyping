72d91b7bf2947eb328e85640622440c9230316d4
emichika <emichika20090401@gmail.com>
2021-01-14 16:34:40 +0900

    保守対応 表示データの更新 OneHandedTyping の完成

--- js/common.js
+++ js/common.js
@@ -1,4 +1,3 @@
-// 参考 https://proverb-encyclopedia.com/subarasi/
 // 日本語リスト
 const japaneseList = [
   "青は藍より出でて藍より青し",
@@ -29,6 +28,44 @@ const japaneseList = [
   "好機逸すべからず",
   "弘法筆を選ばず",
   "虚仮の一心",
+  "酒は憂いの玉箒",
+  "酒は天の美禄",
+  "山椒は小粒でもぴりりと辛い",
+  "三人寄れば文殊の知恵",
+  "自家薬籠中の物",
+  "死せる孔明、生ける仲達を走らす",
+  "蛇は寸にして人を吞む",
+  "酸いも甘いも嚙み分ける",
+  "好きこそ物の上手なれ",
+  "切磋琢磨",
+  "栴檀は双葉より芳し",
+  "千里の道も一歩から",
+  "双璧",
+  "知恵は万代の宝",
+  "掉尾を飾る",
+  "使ている鍬は光る",
+  "敵もさるもの引っ搔くもの",
+  "天衣無縫",
+  "頭角を現す",
+  "堂に入る",
+  "能ある鷹は爪を隠す",
+  "嚢中の錐",
+  "掃き溜めに鶴",
+  "白眉",
+  "八面六臂",
+  "花も実もある",
+  "万緑叢中紅一点",
+  "深い川は静かに流れる",
+  "実ほど頭の下がる稲穂かな",
+  "明鏡止水",
+  "名人は人を謗らず",
+  "目から鼻へ抜ける",
+  "有終の美を飾る",
+  "柳絮の才",
+  "両手に花",
+  "瑠璃も玻璃も照らせば光る",
+  "老馬の智",
+  "六根清浄",
 ];
 // ローマ字リスト
 const romajiList = [
@@ -60,13 +97,51 @@ const romajiList = [
   "KOUKIISSUBEKARAZU",
   "KOUBOUHUDEWOERABAZU",
   "KOKENOISSIN",
+  "SAKEHAUREINOTAMAHAHAKI",
+  "SAKEHATENNNOBIROKU",
+  "SANNSYOUHAKOTUBUDEMOPIRIRITOKARAI",
+  "SANNNINNYOREBAMONNJYUNOTIE",
+  "JIKAYAKUROUTYUUNOMONO",
+  "SISERUKOUMEI,IKERUTYUUTATUWOHASIRASU",
+  "JYAHASUNNNISITEHITOWONOMU",
+  "SUIMOAMAIMOKAMIWAKERU",
+  "SUKIKOSOMONONOJYOUZUNARE",
+  "SESSATAKUMA",
+  "SENNDANNHAHUTABAYORIKANBASI",
+  "SENNRINOMITIMOIPPOKARA",
+  "SOUHEKI",
+  "TIEHABANNDAINOTAKARA",
+  "TYOUBIWOKAZARU",
+  "TUKATTEIRUKUWAHAHIKARU",
+  "TEKIMOSARUMONOHIKKAKUMONO",
+  "TENNIMUHOU",
+  "TOUKAKUWOARAWASU",
+  "DOUNIHAIRU",
+  "NOUARUTAKAHATUMEWOKAKUSU",
+  "NOUTYUUNOKIRI",
+  "HAKIDAMENITURU",
+  "HAKUBI",
+  "HATIMENNROPPI",
+  "HANAMOMIMOARU",
+  "BANNRYOKUSOUTYUUKOUITTENN",
+  "HUKAIKAWAHASIZUKANINAGARERU",
+  "MINORUHODOATAMANOSAGARUINAHOKANA",
+  "MEIKYOUSISUI",
+  "MEIJINNHAHITOWOSOSIRAZU",
+  "MEKARAHANAHENUKERU",
+  "YUUSYUUNOBIWOKAZARU",
+  "RYUUJYONOSAI",
+  "RYOUTENIHANA",
+  "RURIMOHARIMOTERASEBAHIKARU",
+  "ROUBANOTI",
+  "ROKKONNSYOUJYOU",
 ];
 // 残り時間
 const remainingTimeVal = 10;
 // コピーライト
 const copr = "&copy; Copyright emichika Inc. All Rights <span id='ver'></span>";
 // バージョン
-const verNum = "2.3.11";
+const verNum = "2.3.12";
 // コピーライトの設定
 function setCopyright() {
   copyright.innerHTML = copr;
