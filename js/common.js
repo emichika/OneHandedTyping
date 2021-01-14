91c1a6dea08c9787793bbb7ce0e12e54a6a1f1c4
emichika <emichika20090401@gmail.com>
2021-01-14 15:22:47 +0900

    保守対応 表示データの更新

--- js/common.js
+++ js/common.js
@@ -11,6 +11,24 @@ const japaneseList = [
   "一騎当千",
   "一頭地を抜く",
   "上には上がある",
+  "打てば響く",
+  "老いたる馬は道を忘れず",
+  "恐れ入り谷の鬼子母神",
+  "鬼に金棒",
+  "終わりよければすべてよし",
+  "温故知新",
+  "快刀乱麻を断つ",
+  "眼光紙背に徹す",
+  "癖ある馬に能あり",
+  "口も八丁手も八丁",
+  "君子危うきに近寄らず",
+  "君子は豹変す",
+  "君子は和して同ぜず、小人は同じて和せず",
+  "鶏群の一鶴",
+  "兄たり難く弟たり難し",
+  "好機逸すべからず",
+  "弘法筆を選ばず",
+  "虚仮の一心",
 ];
 // ローマ字リスト
 const romajiList = [
@@ -24,13 +42,31 @@ const romajiList = [
   "IKKITOUSENN",
   "ITTOUTIWONUKU",
   "UENIHAUEGAARU",
+  "UTEBAHIBIKU",
+  "OITARUUMAHAMITIWOWASUREZU",
+  "OSOREIRIYANOKISIMOJINN",
+  "ONINIKANABOU",
+  "OWARIYOKEREBASUBETEYOSI",
+  "ONKOTISINN",
+  "KAITOURANNMAWOTATU",
+  "GANNKOUSIHAINITESSU",
+  "KUSEARUUMANINOUARI",
+  "KUTIMOHATTYOUTEMOHATTYOU",
+  "KUNNSIAYAUKINITIKAYORAZU",
+  "KUNNSIHAHYOUHENNSU",
+  "KUNNSIHAWASITEDOUZEZU,SYOUJINNHADOUJITEWASEZU",
+  "KEIGUNNNOIKKAKU",
+  "KEITARIGATAKUTEITARIGATASI",
+  "KOUKIISSUBEKARAZU",
+  "KOUBOUHUDEWOERABAZU",
+  "KOKENOISSIN",
 ];
 // 残り時間
 const remainingTimeVal = 10;
 // コピーライト
 const copr = "&copy; Copyright emichika Inc. All Rights <span id='ver'></span>";
 // バージョン
-const verNum = "2.3.5";
+const verNum = "2.3.11";
 // コピーライトの設定
 function setCopyright() {
   copyright.innerHTML = copr;
