9089738d0a5a0b5d870aa6c1520c5cea7a60f161
emichika <emichika20090401@gmail.com>
2021-01-19 13:21:34 +0900

    保守対応 フォルダ構成の変更 バージョンの修正 ver.2.3.7

--- js/common.js
+++ js/common.js
@@ -141,7 +141,7 @@ const remainingTimeVal = 10;
 // コピーライト
 const copr = "&copy; Copyright emichika Inc. All Rights <span id='ver'></span>";
 // バージョン
-const verNum = "2.3.6";
+const verNum = "2.3.7";
 // コピーライトの設定
 function setCopyright() {
   copyright.innerHTML = copr;
